b3f14db2bf96bbd4bfa8e2d19cb29bab5684874a
EC2 Default User <133572109+tatsuya753@users.noreply.github.com>
2023-08-26 03:10:05 +0000

    Add top

--- db/schema.rb
+++ db/schema.rb
@@ -24,18 +24,6 @@ ActiveRecord::Schema.define(version: 2023_08_25_162753) do
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
-  create_table "publics", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_publics_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_publics_on_reset_password_token", unique: true
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
