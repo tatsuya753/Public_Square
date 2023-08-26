913977749740fa0db4b0a118c6f44b758cb90d68
EC2 Default User <133572109+tatsuya753@users.noreply.github.com>
2023-08-26 06:41:01 +0000

    Add post-index

--- db/schema.rb
+++ db/schema.rb
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2023_08_26_045919) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
