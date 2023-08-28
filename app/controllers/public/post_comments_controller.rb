ca7a8a90e736463febd89530659d2da77563c4e5
EC2 Default User <133572109+tatsuya753@users.noreply.github.com>
2023-08-28 09:05:48 +0000

    Add post-show

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -5,7 +5,7 @@ class Public::PostCommentsController < ApplicationController
     comment = current_user.post_comments.new(post_comment_params)
     comment.post_id = post.id
     comment.save
-    redirect_to post_path(post_image)
+    redirect_to post_path(post)
   end
 
   def destroy
