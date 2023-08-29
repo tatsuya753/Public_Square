098aaa2a3c92b939257311d1985fc16b8c6b499c
EC2 Default User <133572109+tatsuya753@users.noreply.github.com>
2023-08-29 02:24:28 +0000

    Add comment_Ajax

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -1,16 +1,16 @@
 class Public::PostCommentsController < ApplicationController
 
   def create
-    post = Post.find(params[:post_id])
+    @post = Post.find(params[:post_id])
     comment = current_user.post_comments.new(post_comment_params)
-    comment.post_id = post.id
+    comment.post_id = @post.id
     comment.save
-    redirect_to post_path(post)
   end
 
   def destroy
-    PostComment.find(params[:id]).destroy
-    redirect_to post_path(params[:post_id])
+    @post_comment = PostComment.find(params[:id]).destroy
+    @post_comment.destroy
+    @post = Post.find(params[:post_id])
   end
 
   private
